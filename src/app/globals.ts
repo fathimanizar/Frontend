96ea7a863b22557cf5565713d840a2f8cdd11d98
Fathima Nizar <fathimaen@gmail.com>
2022-09-26 00:17:36 +0530

    frontend changes

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,2 +1,2 @@
-// export const server_address: string = '/api';
-export const server_address: string = 'http://localhost:3000/api';
+export const server_address: string = '/api';
+// export const server_address: string = 'http://localhost:3000/api';
